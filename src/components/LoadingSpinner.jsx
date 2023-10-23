e919e2d6f7746d16de2f01766d8e9a1d56529d8a
shakilahmedatik <shakilatik@gmail.com>
2023-10-24 02:58:16 +0600

    loading state added

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoadingSpinner = () => {
+  return (
+    <div className='w-16 h-16 border-4 border-dashed rounded-full animate-spin border-primary'></div>
+  )
+}
+
+export default LoadingSpinner
--- src/pages/Memories.jsx
+++ src/pages/Memories.jsx
@@ -1,13 +1,26 @@
 import MemoryCard from '../components/MemoryCard'
 import { getMemories } from '../api/memories'
 import { useQuery } from '@tanstack/react-query'
+import EmptyState from '../components/EmptyState'
+import LoadingSpinner from '../components/LoadingSpinner'
 
 const Memories = () => {
   const { data: memories, isLoading } = useQuery({
     queryFn: async () => await getMemories(),
     queryKey: ['memories'],
   })
-  if (isLoading) return <div>Loading</div>
+  if (isLoading)
+    return (
+      <div className='flex justify-center items-center pt-[18%]'>
+        <LoadingSpinner />
+      </div>
+    )
+  if (memories.length === 0)
+    return (
+      <div className='flex justify-center items-center pt-12'>
+        <EmptyState />
+      </div>
+    )
   return (
     <div className='w-full flex flex-column justify-center  px-4 sm:px-8 lg:px-12'>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 pt-[5%]'>
