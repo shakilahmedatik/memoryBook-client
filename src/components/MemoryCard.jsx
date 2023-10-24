17a8fee5bd4b68c2a22c16c7d9e2a7f7297cdf91
shakilahmedatik <shakilatik@gmail.com>
2023-10-24 11:56:33 +0600

    delete button added in cards

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,10 +2,13 @@
 const MemoryCard = ({ memory }) => {
   console.log(memory)
   return (
-    <div className='card w-84 h-48 bg-primary text-primary-content'>
+    <div className='card w-84 h-48 bg-primary text-primary-content hover:bg-secondary relative'>
       <div className='card-body'>
         <h2 className='card-title'>{memory.title}</h2>
         <p>{memory.description}</p>
+        <div className='cursor-pointer absolute right-0 top-0'>
+          <kbd className='kbd bg-red-500 rounded-xl text-white'>X</kbd>
+        </div>
       </div>
     </div>
   )
